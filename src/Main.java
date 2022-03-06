e9242c01ae3ddbe1a295aff086a4f1e678358faa
Jakub Gramburg <jacubgr@gmail.com>
2022-03-06 11:01:55 +0100

    zad01 - feeding animal

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws Exception {
+
+        Animal dog1 = new Animal(Animal.AnimalType.DOG);
+        System.out.println("Weight of dog1:" + dog1.getWeight());
+
+        dog1.feed();
+        dog1.feed();
+        System.out.println("Weight of dog1 after feeding:" + dog1.getWeight());
+
+        var totalWalks = 6;
+        for (int i = 0; i < totalWalks; i++) {
+            dog1.takeForAWalk();
+        }
+
+        System.out.println("Weight of dog1:" + dog1.getWeight());
+
+        dog1.takeForAWalk();
+
     }
 }
