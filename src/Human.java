fc9cc47dcfc5b57d41ccae357bb1f48f3f865114
Jakub Gramburg <jacubgr@gmail.com>
2022-03-06 14:24:23 +0100

    zad03 - salary getter and setter

--- src/Human.java
+++ src/Human.java
@@ -1,12 +1,23 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Human {
 
     private final String firstName;
     private final String lastName;
+    private Double salary;
+    private final Map<LocalDateTime, Double> salaryQueries;
+
+    private static final DateTimeFormatter DTF = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+
     Car car;
 
     public Human(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.salaryQueries = new HashMap<>();
     }
 
     public void addCar(Car car1) throws Exception {
@@ -19,5 +30,34 @@ public class Human {
 
     public String getFullName() {
         return firstName + " " + lastName;
+    }
+
+    public Double getSalary() {
+        if (!salaryQueries.isEmpty()) {
+            System.out.println("\n ------------");
+            System.out.println("| Salary queries:");
+
+            for (var query : salaryQueries.entrySet()) {
+                System.out.println("| Query at " + DTF.format(query.getKey()) + " returned salary: " + query.getValue());
+            }
+            System.out.println(" ------------");
+        }
+        salaryQueries.put(LocalDateTime.now(), salary);
+        return this.salary;
+    }
+
+    public void setSalary(Double salary) throws Exception {
+        if (salary < 0) {
+            throw new Exception("Salary cannot be negative");
+        }
+
+        LocalDateTime createdDate = LocalDateTime.now();
+
+        System.out.println("\n ------------");
+        System.out.println("| New salary " + salary + " has been saved!  || " + DTF.format(createdDate));
+        System.out.println("| Please visit Halina from HR to get your copy of doc. " +
+                "PS. Pegasus knows everything ( \u0361\u00B0 \u035C\u0296 \u0361\u00B0)");
+        System.out.println(" ------------");
+        this.salary = salary;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,27 @@
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    static Human me;
+    static Car car1;
 
-        Human me = new Human("Jakub", "Gramburg");
-        Car car1 = new Car("Ford", "Focus");
+    public static void main(String[] args) throws Exception {
+        me = new Human("Jakub", "Gramburg");
+        car1 = new Car("Ford", "Focus");
 
         me.addCar(car1);
 
+        // zad03();
+
+
+    }
+
+    private static void zad03() throws Exception {
         System.out.println(me.getFullName() + " has a car: " + me.car.getName());
+        me.setSalary(10000.0);
+        // me.setSalary(-3000.0);
+
+        me.getSalary();
+        me.getSalary();
+        me.setSalary(15000.0);
+        System.out.println(me.getFullName() + " has a salary: " + me.getSalary());
     }
 }
